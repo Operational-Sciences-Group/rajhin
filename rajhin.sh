5499efa96971559b4ef973da36970d6940d88f7b
JoustingZebra <80557158+JoustingZebra@users.noreply.github.com>
2022-03-23 21:36:31 -0400

    Rajhin.sh V1.1 candidate
    Added:
    - Privilege check before execution
    - Several OUIs
    - Now enables RFC 4941 (IPv6 Privacy extensions). The SLAAC Ipv6 address is
      no longer derived from the EUI-48. Uses temporary, pseudo-random Ipv6
      addresses in the absence of a DHCP server.
    - Now enables RFC 7217. SLAAC IPv6 addresses configured using this method are
       stable within each subnet, but the corresponding Interface Identifier
       changes when the host moves from one network to another.
    
    Improved:
    
    - No longer assigns the same EUI-48 to multiple interfaces
    - No longer turns on network adapters that were off when the script was run.
    - Removed dependency on "basename", even though it is a GNU coreutil.
    - Removed repetitive variables
    - Changed to "/dev/urandom" to "/dev/random" for older distro security.

--- rajhin.sh
+++ rajhin.sh
@@ -4,13 +4,49 @@
 #    ______      ____    ____        ____   ____            ____           ____  ____  
 #  >   /  /___/ /___    /___/ /   / /___/  /___/  / / \  / / __    /    / /___/ /___/ <
 # >   /  /   / /___    /     /___/ /   \  /   \  / /   \/ /___/   /__  / /   / /   \ <
-d1=$(tr -dc 'A-F0-9' < /dev/urandom | head -c2)
-d2=$(tr -dc 'A-F0-9' < /dev/urandom | head -c2)
-d3=$(tr -dc 'A-F0-9' < /dev/urandom | head -c2)
-interfaces=($(basename -a /sys/class/net/* | grep -v lo))
+
+sudo echo "Executing Rajhin"
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+function RandHex(){
+	OneByte=$(tr -dc 'A-F0-9' < /dev/random | head -c2)
+	echo "$OneByte"
+}
+
+# Add list of vendors
+declare -a OUIarray=(00:50:E4 00:56:CD 10:A5:1D 00:e0:4c)
+OUI=$(shuf -n1 -e "${OUIarray[@]}")
+
+# Cryptographically secure, because you know it.
+SecretKey=$(uuidgen | sed "s/-//g; s/..../:&/g; s/^://")
+
+# Enforce RFC 4941 (AKA "Privacy Extensions") 
+# This decouples the last three fields of the IPv6 StateLess Address Auto-Config (SLAAC) from the EUI-48 on default net adapters. 
+sysctl -w net.ipv6.conf.default.use_tempaddr=2
+
+
+# Enable RFC 7217 (AKA "Semantically Opaque Interface Identifiers")
+# This makes the IPv6 SLAAC from an opaque hash.
+# This address is still "static" per network.
+# Yes, it changes every time you run the script.
+
+sysctl -w net.ipv6.conf.default.stable_secret="$SecretKey"
+
+interfaces=($(find /sys/class/net/* | grep -v "lo" | cut -d'/' -f5))
 for i in "${interfaces[@]}"
 do
-	ifconfig "$i" down
-	ifconfig "$i" hw ether 00:e0:4c:"$d1":"$d2":"$d3"
-	ifconfig "$i" up
+	# of the interface is up
+	AdapterStatus=$(cat /sys/class/net/"$i"/operstate)
+	if [[ $AdapterStatus == "up" ]]; then
+		ifconfig "$i" down
+		ifconfig "$i" hw ether "$OUI":"$(RandHex)":"$(RandHex)":"$(RandHex)"
+		ifconfig "$i" up
+		echo New EUI-48 for "$i" = "$OUI":"$(RandHex)":"$(RandHex)":"$(RandHex)"
+	fi
 done
+
